e55e45cfa9c494de29c8cc55773acb699cced547
Ariel Walters <arieldwalters4@gmail.com>
2025-10-22 01:39:27 -0500

    feat: add Vercel Analytics
      - Added @vercel/analytics for page tracking and performance monitoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import WellWaltStudios from "./WellWaltStudios";
 import Blog from "./Blog";
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<WellWaltStudios />} />
           <Route path="/blog" element={<Blog />} />
         </Routes>
+        <Analytics />
       </div>
     </Router>
   );
